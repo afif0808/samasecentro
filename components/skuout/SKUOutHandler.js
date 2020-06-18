31b97fcb3de724176b10bfec05be6df924fe132f
Just Another Fif <31957520+afif0808@users.noreply.github.com>
2020-06-18 09:27:33 +0700

    Add files via upload

---
+++ components/skuout/SKUOutHandler.js
@@ -0,0 +1,22 @@
+function CreateSKUOuts(skuOuts,skuOutGroup) {
+    var postData = {
+        sku_outs : skuOuts , 
+        sku_out_group : skuOutGroup,
+    }
+    var req = {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(postData)
+    }
+    console.log(req)
+
+    return fetch("http://localhost:555/skuouts",req).then(resp => resp.json())
+}
+
+function GetSKUOutsByGroupID(groupId) {
+    return fetch("http://localhost:555/skuoutgroups/" + groupId + "/skuouts").then((resp) => resp.json())
+}
+
+export {CreateSKUOuts , GetSKUOutsByGroupID}
